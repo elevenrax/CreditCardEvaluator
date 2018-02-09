96ead53ff0408608e42ea6f6b78199e77e217b25
Nate <nate@11rax.com>
2018-02-09 16:07:17 +1100

    Main: Add some sample code how to use

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Card.CreditCard;
+import Card.CreditCardBrandDeterminer;
 import Validator.CreditCardValidator;
 import ValidityChecks.CardNumberWellFormedValidator;
 import ValidityChecks.LuhnCheckValidator;
@@ -11,22 +12,28 @@ public class Main {
          */
 
         // Make a new CreditCard
+        // Will throw a RuntimeException if the card is not a known brand.
         CreditCard card = new CreditCard("222100345345");
 
-        // What Brand is it?
+        // What Brand is the card?
         System.out.println( card.getCreditCardBrand() );
 
-        // Is the Card Valid?
+        // Access ranges for CreditCard Brands
+        System.out.println("Visa range is : " + CreditCardBrandDeterminer.VISA_MIN +
+                " to " + CreditCardBrandDeterminer.VISA_MAX);
+
+        // Is the Card Valid? This validates the card against ALL criteria.
+        // A card is only valid if it passes all the tests.
         CreditCardValidator validator = new CreditCardValidator(card);
         System.out.println("Is Card Valid? " + validator.isValid());
 
-        // Granular Validity
+        // Granular Validity - Test if the card is well formed, this includes:
+        // No letters, no numbers starting with a zero, and between 12 - 19 digits
         boolean numberWellFormed = CardNumberWellFormedValidator.validate(card);
         System.out.println("Is Number Well Formed? " + numberWellFormed);
 
         boolean luhnCheckSuccess = new LuhnCheckValidator(card).validate();
         System.out.println("Is Successful in LuhnCheck? " + luhnCheckSuccess);
-
     }
 
 }
