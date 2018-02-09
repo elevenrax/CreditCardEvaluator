00919e1d69cbda77f9b775d2e39bfb55747d18a2
Nate <nate@11rax.com>
2018-02-09 16:07:02 +1100

    CreditCardBrandDeterminer: add card ranges as const data members

--- src/Card/CreditCardBrandDeterminer.java
+++ src/Card/CreditCardBrandDeterminer.java
@@ -4,6 +4,7 @@ package Card;
  *  A credit card is branded as follows:
  *     VISA:            400000 - 499999
  *     MasterCard:      222100 - 272099
+ *                      510000 - 559999
  *     ChinaUnionPay:   620000 - 629999
  *     Maestro          500000 - 509999
  *                      560000 - 629999
@@ -12,29 +13,51 @@ package Card;
  */
 public class CreditCardBrandDeterminer {
 
+
+    // Constants for card range
+    public static final int VISA_MIN = 400000;
+    public static final int VISA_MAX = 499999;
+
+    public static final int MASTERCARD_MIN_RANGE_ONE = 222100;
+    public static final int MASTERCARD_MAX_RANGE_ONE = 272099;
+
+    public static final int MASTERCARD_MIN_RANGE_TWO = 510000;
+    public static final int MASTERCARD_MAX_RANGE_TWO = 559999;
+
+    public static final int CUP_MIN = 620000;
+    public static final int CUP_MAX = 629999;
+
+    public static final int MAESTRO_MIN_RANGE_ONE = 500000;
+    public static final int MAESTRO_MAX_RANGE_ONE = 509999;
+
+    public static final int MAESTRO_MIN_RANGE_TWO = 560000;
+    public static final int MAESTRO_MAX_RANGE_TWO = 699999;
+
+
+
     public static CreditCard.CardBrand determineBrand(CreditCard card) {
         int brandNumber = card.getFirstSixDigitsForBrandIdentification();
         CreditCard.CardBrand brand;
 
         // VISA
-        if ( numberInRange(brandNumber, 400000, 499999) ) {
+        if ( numberInRange(brandNumber, VISA_MIN, VISA_MAX) ) {
             brand = CreditCard.CardBrand.VISA;
         }
 
         // MASTERCARD
-        else if ( numberInRange(brandNumber, 222100, 272099)
-                  || numberInRange(brandNumber, 510000, 559999) ) {
+        else if ( numberInRange(brandNumber, MASTERCARD_MIN_RANGE_ONE, MASTERCARD_MAX_RANGE_ONE)
+                || numberInRange(brandNumber, MASTERCARD_MIN_RANGE_TWO, MASTERCARD_MAX_RANGE_TWO) ) {
             brand = CreditCard.CardBrand.MASTERCARD;
         }
 
         // CHINA UNION PAY (CUP) NB. overlaps with Maestro, but takes priority.
-        else if ( numberInRange(brandNumber, 620000, 629999) ) {
+        else if ( numberInRange(brandNumber, CUP_MIN, CUP_MAX) ) {
             brand = CreditCard.CardBrand.CHINA_UNION_PAY;
         }
 
         // MAESTRO
-        else if ( numberInRange(brandNumber, 500000, 509999)
-                || numberInRange(brandNumber, 560000, 699999) ) {
+        else if ( numberInRange(brandNumber, MAESTRO_MIN_RANGE_ONE, MAESTRO_MAX_RANGE_ONE)
+                || numberInRange(brandNumber, MAESTRO_MIN_RANGE_TWO, MAESTRO_MAX_RANGE_TWO) ) {
             brand = CreditCard.CardBrand.MAESTRO;
         }
 
